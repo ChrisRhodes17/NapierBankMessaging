9f8558e734fb471dc5e760466d9f846bd8ea28f1
chris <cdrhodes17@gmail.com>
2021-11-24 15:59:38 +0000

    Protype

--- NapierBankMessaging/Form1.Designer.cs
+++ NapierBankMessaging/Form1.Designer.cs
@@ -29,13 +29,152 @@ namespace NapierBankMessaging
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHeader = new System.Windows.Forms.TextBox();
+            this.txtMessIn = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtMessOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(807, 25);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(389, 426);
+            this.txtList.TabIndex = 23;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(542, 381);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(128, 38);
+            this.btnDisplay.TabIndex = 22;
+            this.btnDisplay.Text = "Display Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 20);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Processed Body Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Message Body Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Message Header";
+            // 
+            // txtHeader
+            // 
+            this.txtHeader.Location = new System.Drawing.Point(12, 43);
+            this.txtHeader.Multiline = true;
+            this.txtHeader.Name = "txtHeader";
+            this.txtHeader.Size = new System.Drawing.Size(387, 60);
+            this.txtHeader.TabIndex = 15;
+            // 
+            // txtMessIn
+            // 
+            this.txtMessIn.Location = new System.Drawing.Point(12, 152);
+            this.txtMessIn.Multiline = true;
+            this.txtMessIn.Name = "txtMessIn";
+            this.txtMessIn.Size = new System.Drawing.Size(387, 206);
+            this.txtMessIn.TabIndex = 14;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(340, 381);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(90, 38);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(137, 381);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(90, 38);
+            this.btnProcess.TabIndex = 12;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtMessOut
+            // 
+            this.txtMessOut.Location = new System.Drawing.Point(413, 48);
+            this.txtMessOut.Multiline = true;
+            this.txtMessOut.Name = "txtMessOut";
+            this.txtMessOut.Size = new System.Drawing.Size(387, 310);
+            this.txtMessOut.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(803, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "List Display";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1208, 463);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessOut);
+            this.Controls.Add(this.txtHeader);
+            this.Controls.Add(this.txtMessIn);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHeader;
+        private System.Windows.Forms.TextBox txtMessIn;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtMessOut;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- NapierBankMessaging/Form1.cs
+++ NapierBankMessaging/Form1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace NapierBankMessaging
 {
@@ -14,7 +18,297 @@ namespace NapierBankMessaging
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+        }
+
+
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            string path = Path.GetFullPath(Path.Combine(currentPath, @"..\..\..\..\"));
+            
+
+            string messageIn = txtMessIn.Text;
+            string messageHead = txtHeader.Text;
+            string messageOut = "";
+
+
+            
+            if (txtHeader.Text == "")
+            {
+                txtMessOut.Text = "You need to add a Message Header";
+            }
+            else
+            {
+                string messageType = messageHead.Substring(0, 1);
+
+                if (messageType == "S")
+                {
+                    //call sms, need to make this a class
+                    ProcessSMS(messageIn, ref messageOut, path);
+                }
+                else if (messageType == "E")
+                {
+                    //call email
+                    ProcessEmail(messageIn, ref messageOut, path);
+                }
+                else if (messageType == "T")
+                {
+                    //call tweets
+                    ProcessTweet(messageIn, ref messageOut, path);
+                }
+                else
+                {
+                    txtMessOut.Text = "Invalid Message Header";
+                }
+
+                txtMessOut.Text = messageOut;
+
+            }
+
+        }
+
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMessIn.Text = "";
+            txtMessOut.Text = "";
+            txtHeader.Text = "";
+            txtList.Text = "";
+        }
+
+
+        public void ProcessSMS(string messageIn, ref string messageOut, string path)
+        {
+            string[] textAbbreviation = { };
+            string[] textWords = { };
+
+            List<string> listAbb = new List<string>();
+            List<string> listWords = new List<string>();
+
+
+            int counter = 0;
+
+            // Read the file line by line.  
+            foreach (string line in System.IO.File.ReadLines(@path + "textwords.csv"))
+            {
+                string[] columns = line.Split(',');
+                listAbb.Add(columns[0]);
+                listWords.Add(columns[1]);
+                counter++;
+            }
+
+            textAbbreviation = listAbb.ToArray();
+            textWords = listWords.ToArray();
+
+
+            string[] input = messageIn.Split(' ');
+
+
+            for (int i = 0; i < (textAbbreviation.Length); i++)
+            {
+                for (int j = 0; j < (input.Length); j++)
+                {
+                    if (input[j] == textAbbreviation[i])
+                    {
+                        input[j] = input[j] + " <" + textWords[i] + ">";
+                    }
+
+                }
+
+            }
+
+            for (int i = 0; i < (input.Length); i++)
+            {
+                messageOut = messageOut + input[i] + " ";
+            }
+
+        }
+
+        public void ProcessEmail(string messageIn, ref string messageOut, string path)
+        {
+
+            
+
+            StreamWriter sw = new StreamWriter(path + "quarantineList.txt", true);
+            StreamWriter swSIR = new StreamWriter(path + "SIR.txt", true);
+
+            string email = @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)";
+
+            String[] lines;
+
+
+            lines = Regex.Split(messageIn, @"\r\n|\r|\n");
+            string sir = lines[0].Substring(0, 10);
+
+            if (sir == "Sort Code:")
+            {
+                swSIR.WriteLine(lines[0]);                
+                swSIR.WriteLine(lines[1]);
+            }
+            swSIR.Close();
+
+
+
+
+
+            foreach (Match item in Regex.Matches(messageIn, email))
+            {
+                sw.WriteLine(item);
+            }
+            sw.Close();
+
+            messageOut = Regex.Replace(messageIn, email, "<URL Quarantined>");
+
+            
+
+
+            
+
+            
+            
+            
+
+
+        }
+
+
+        public void ProcessTweet(string messageIn, ref string messageOut, string path)
+        {
+            StreamWriter swHashtag = new StreamWriter(path + "hashtagList.txt", true);
+            StreamWriter swMentions = new StreamWriter(path + "mentionsList.txt", true);
+
+            string[] textAbbreviation = { };
+            string[] textWords = { };
+
+            List<string> listAbb = new List<string>();
+            List<string> listWords = new List<string>();
+
+
+            int counter = 0;
+
+            // Read the file line by line.  
+            foreach (string line in System.IO.File.ReadLines(@path + "textwords.csv"))
+            {
+                string[] columns = line.Split(',');
+                listAbb.Add(columns[0]);
+                listWords.Add(columns[1]);
+                counter++;
+            }
+
+            textAbbreviation = listAbb.ToArray();
+            textWords = listWords.ToArray();
+
+
+            string[] input = Regex.Split(messageIn, @"\r\n|\r|\n| ");
+
+
+
+
+            for (int i = 0; i < (input.Length); i++)
+            {
+                for (int j = 0; j < (textAbbreviation.Length); j++)
+                {
+                    if (input[i] == textAbbreviation[j])
+                    {
+                        input[i] = input[i] + " <" + textWords[j] + ">";
+                    }
+                }
+
+                string firstDiget = input[i].Substring(0, 1);
+
+                if (firstDiget == "#")
+                {
+                    swHashtag.WriteLine(input[i]);
+                }
+                if (firstDiget == "@")
+                {
+                    swMentions.WriteLine(input[i]);
+                }
+            }
+
+            swHashtag.Close();
+            swMentions.Close();
+
+            for (int i = 0; i < (input.Length); i++)
+            {
+                messageOut = messageOut + input[i] + " ";
+            }
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            
+            string currentPath = Directory.GetCurrentDirectory();
+            string path = Path.GetFullPath(Path.Combine(currentPath, @"..\..\..\..\"));
+
+            txtList.Text = "";
+
+            
+            txtList.AppendText("Trending List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@path + "hashtagList.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+            
+
+            txtList.AppendText("\r\n" + "Mentions List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\chris\OneDrive\Documents\3rdYearSoftwareEngineeringCW\mentionsList.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+            
+
+            txtList.AppendText("\r\n" + "SIR List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\chris\OneDrive\Documents\3rdYearSoftwareEngineeringCW\SIR.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+            
+             
+
+            txtList.Text = "Help MEEEEEE!!!!!";
+
+        }
+
+
+
+
+        
+
+        private void btnDisplay_Click_1(object sender, EventArgs e)
+        {
+            
+            string currentPath = Directory.GetCurrentDirectory();
+            string path = Path.GetFullPath(Path.Combine(currentPath, @"..\..\..\..\"));
+
+            txtList.Text = "";
+
+            
+            txtList.AppendText("Trending List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@path + "hashtagList.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+            
+
+            txtList.AppendText("\r\n" + "Mentions List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@path + "mentionsList.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+            
+
+            txtList.AppendText("\r\n" + "SIR List" + "\r\n");
+            foreach (string line in System.IO.File.ReadLines(@path + "SIR.txt"))
+            {
+                txtList.AppendText(line + "\r\n");
+            }
+
         }
     }
 }
